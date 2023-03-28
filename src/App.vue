c5c2dd3d27c9b38d0939f73376a8abeae04285d0
Alan <cxy814941789@gmail.com>
2023-03-29 02:18:36 +0800

    创建项目

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</template>
+  <h1>Hello.</h1>
+  <h2>This is my home page</h2>
+  </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 }
 </script>
 
